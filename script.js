a1f5270057d8938ca09bcbf8e164999456c33b7f
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-06-05 08:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function mincost(arr) {
-  arr.sort((a, b) => a - b);
+  if (arr.length <= 1) return 0;
 
+  arr.sort((a, b) => a - b);
   let totalCost = 0;
 
   while (arr.length > 1) {
     let first = arr.shift();
     let second = arr.shift();
-
     let cost = first + second;
     totalCost += cost;
 
+    // Insert back in sorted order (to simulate a min-heap)
     let i = 0;
     while (i < arr.length && arr[i] < cost) i++;
     arr.splice(i, 0, cost);
@@ -17,4 +18,6 @@ function mincost(arr) {
 
   return totalCost;
 }
+
+module.exports = mincost;
 
